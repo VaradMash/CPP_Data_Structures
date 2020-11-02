cb111f51ddcbfa3d6066f07f348b419692dd32db
VaradMash <VaradMash@users.noreply.github.com>
2020-11-02 07:46:55 +0530

    Queue implementation using LinkedList added.

---
+++ queue_using_LinkedList/Queue.h
@@ -0,0 +1,38 @@
+/*
+ * Queue.h
+ *
+ *  Created on: 27-Oct-2020
+ *      Author: varadm
+ */
+
+
+#ifndef SRC_QUEUE_H_
+#define SRC_QUEUE_H_
+
+//Node
+template <typename T>
+class QNode
+{
+public:
+	T data;
+	QNode* next;
+};
+
+//Queue class
+template <typename T>
+class Queue
+{
+private:
+	QNode<T>* front;
+	QNode<T>* rear;
+public:
+	T data;				//Data member for internal transaction
+	Queue();			//Constructor
+	~Queue();			//Destructor
+	bool isEmpty();		//Method to identify isEmpty condition
+	void enqueue(T);	//Method to enqueue data
+	T dequeue();		//Method to dequeue data
+	void display();		//Method to display queue
+};
+
+#endif /* SRC_QUEUE_H_ */
